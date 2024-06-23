e47702db40aaf057d0c9bac10e56835efb2cc638
nathaniafadlilah14 <nathaniafadlilah@student.telkomuniversity.ac.id>
2024-06-24 00:48:01 +0700

    modul8

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+Auth::routes(['register' => false]);
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile');
+Route::resource('employees', EmployeeController::class);    
+});
+
+
+
+
+
+
